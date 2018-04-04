fb0ee496862d8ecbaa569fd215f0ae72184a4671
Scott Wakeman <github email address>
2018-04-04 15:59:33 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new
+    @user.password = params[:user][:password]
+    @user.name = params[:user][:name]
+    if @user.save
+      session[:user_id] = @user.id
+      session[:user_name] = @user.name
+      # UsersController create logs you in
+        redirect_to new_user_path
+    else
+      render '/'
+    end
+  end
+
   def visit_signup
-    redirect_to "/user/new"
+    redirect_to "/users/new"
+  end
+
+  def visit_signin
+    redirect_to "/users/edit"
   end
 end
