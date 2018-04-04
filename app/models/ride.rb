cc71f0a0b5a07d1787aecc3e40e2e73fcbbd19b2
Scott Wakeman <github email address>
2018-04-03 17:06:59 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,13 +7,40 @@ class Ride < ActiveRecord::Base
   validates :user_id, presence: true
   validates :attraction_id, presence: true
 
-  def take_ride
-    #it "has a method 'take_ride' that accounts for the user not having enough tickets" do
-    #it "has a method 'take_ride' that accounts for the user not being tall enough" do
-    #it "has a method 'take_ride' that accounts for the user not being tall enough and not having enough tickets" do
-    #it "has a method 'take_ride' that updates ticket number" do
-    #it "has a method 'take_ride' that updates the user's nausea" do
-    #it "has a method 'take_ride' that updates the user's happiness" do
+  def enough_tickets
+    self.user.tickets >= self.attraction.tickets
   end
 
+  def height_requirement
+    self.user.height >= self.attraction.min_height
+  end
+
+  def take_ride
+    #it "has a method 'take_ride' that accounts for the user not having enough tickets" do
+    #user.tickets < attraction.tickets
+    #self.user.tickets.count < attraction.tickets
+
+
+    if !enough_tickets && !height_requirement
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+
+    elsif !enough_tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+
+    #it "has a method 'take_ride' that accounts for the user not being tall enough" do
+    elsif !height_requirement
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+
+
+    end
+
+    #it "has a method 'take_ride' that updates ticket number" do
+
+    #it "has a method 'take_ride' that updates the user's nausea" do
+    #user.nausea == 5
+
+    #it "has a method 'take_ride' that updates the user's happiness" do
+    #user.happiness == 3
+    #end
+  end
 end
